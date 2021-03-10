79108ee8b469ef13a343d3a23dd3012257372433
Shraddha Gajul <shraddha.gajul.27@gmail.com>
2021-03-10 13:20:14 +0530

    Learn interface

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -19,14 +19,31 @@ var (
 
 func init(){
 	 UserDao = &userDao{}
+	 	 UserDao = &userDao1{}
+
 }
 type userDaoInterface interface{
 	GetUser(userId int64) (*User, *utils.ApplicationError)
 }
 
 type userDao struct{}
+type userDao1 struct{}
 
 func (u *userDao)GetUser(userId int64) (*User, *utils.ApplicationError) {
+	log.Println("we are accessing the database.")
+	if user := users[userId] ;user != nil {
+		return user,nil
+		
+	}
+	
+	return nil, &utils.ApplicationError{
+		Message : fmt.Sprintf("user %v does not exists", userId),
+		StatusCode : http.StatusNotFound,
+		Code : "not_found",
+	}
+}
+
+func (u *userDao1)GetUser(userId int64) (*User, *utils.ApplicationError) {
 	log.Println("we are accessing the database.")
 	if user := users[userId] ;user != nil {
 		return user,nil
